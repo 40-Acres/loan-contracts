2b18c89ad4b0c7157166f16407308e1eea520d79
De'on Summers <deonsummers91@gmail.com>
2025-10-12 10:32:01 -0500

    use authorized sender

--- src/facets/account/XRexFacet.sol
+++ src/facets/account/XRexFacet.sol
@@ -84,8 +84,9 @@ contract XRexFacet {
     }
 
     function xRexClaim(address loanContract, address[] calldata fees, address[][] calldata tokens, bytes calldata tradeData, uint256[2] calldata allocations) external onlyApprovedContract(loanContract) returns (uint256) {
+        require(msg.sender == _portfolioFactory.getAccountOwner(address(this)) || _accountConfigStorage.isAuthorizedCaller(msg.sender));
+
         IVoteModule(_voteModule).setAdmin(address(loanContract));
-        require(msg.sender == _entryPoint);
         uint256 result = IXLoan(loanContract).claim(fees, tokens, tradeData, allocations);
         IVoteModule(_voteModule).setAdmin(address(0));
         if(allocations[1] > 0) {
