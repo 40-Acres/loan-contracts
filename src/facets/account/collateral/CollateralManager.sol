35d21e5f903af84f4855e220affda686b7c449d2
De'on Summers <deonsummers91@gmail.com>
2026-01-16 12:31:44 -0600

    fix Unsafe signed to unsigned cast

--- src/facets/account/collateral/CollateralManager.sol
+++ src/facets/account/collateral/CollateralManager.sol
@@ -110,6 +110,7 @@ library CollateralManager {
 
         (, uint256 previousMaxLoanIgnoreSupply) = getMaxLoan(portfolioAccountConfig);
         int128 newLockedCollateralInt = IVotingEscrow(address(ve)).locked(tokenId).amount;
+        require(newLockedCollateralInt >= 0, "Locked collateral amount must be greater than 0");
         uint256 newLockedCollateral = uint256(uint128(newLockedCollateralInt));
         if(newLockedCollateral > previousLockedCollateral) {
             uint256 difference = newLockedCollateral - previousLockedCollateral;
