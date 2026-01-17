6268ba0c8d703db62e07666e2a27b4c889a8590c
Deon Summers <deonsummers91@gmail.com>
2026-01-17 13:45:48 -0600

    use constructor args (#187)

--- src/facets/account/claim/SuperchainClaimingFacet.sol
+++ src/facets/account/claim/SuperchainClaimingFacet.sol
@@ -5,27 +5,30 @@ import {ClaimingFacet} from "./ClaimingFacet.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract SuperchainClaimingFacet is ClaimingFacet {
-    constructor(address portfolioFactory, address portfolioAccountConfig, address votingEscrow, address voter, address rewardsDistributor, address loanConfig, address swapConfig, address vault)
+    address public immutable _weth;
+
+    constructor(address portfolioFactory, address portfolioAccountConfig, address votingEscrow, address voter, address rewardsDistributor, address loanConfig, address swapConfig, address vault, address weth)
         ClaimingFacet(portfolioFactory, portfolioAccountConfig, votingEscrow, voter, rewardsDistributor, loanConfig, swapConfig, vault)
     {
+        _weth = weth;
     }
 
 
     function claimFees(address[] calldata fees, address[][] calldata tokens, uint256 tokenId) public override {
         // get tx.origin weth balance
-        uint256 preWethBalance = IERC20(address(0x4200000000000000000000000000000000000006)).balanceOf(msg.sender);
+        uint256 preWethBalance = IERC20(_weth).balanceOf(msg.sender);
         super.claimFees(fees, tokens, tokenId);
-        uint256 postWethBalance = IERC20(address(0x4200000000000000000000000000000000000006)).balanceOf(msg.sender);
+        uint256 postWethBalance = IERC20(_weth).balanceOf(msg.sender);
 
         // any difference balance should be sent from this address
         uint256 difference = preWethBalance - postWethBalance;
         if(difference > 0) {
-            uint256 portfolioWethBalance = IERC20(address(0x4200000000000000000000000000000000000006)).balanceOf(address(this));
+            uint256 portfolioWethBalance = IERC20(_weth).balanceOf(address(this));
             // if the difference is greater than the portfolio weth balance, send the entire portfolio weth balance
             if(difference > portfolioWethBalance) {
                 difference = portfolioWethBalance;
             }
-            IERC20(address(0x4200000000000000000000000000000000000006)).transfer(msg.sender, difference);
+            IERC20(_weth).transfer(msg.sender, difference);
         }
     }
 }
