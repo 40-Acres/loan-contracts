aabfd6d20259b8beb7cdb1ae076b948e12971ed5
Deon Summers <deonsummers91@gmail.com>
2026-01-16 18:52:52 -0600

    implement delegated voting (#181)

--- src/facets/account/vote/interfaces/IVotingFacet.sol
+++ src/facets/account/vote/interfaces/IVotingFacet.sol
@@ -7,4 +7,5 @@ pragma solidity ^0.8.28;
  */
 interface IVotingFacet {
     function vote(uint256 tokenId, address[] calldata pools, uint256[] calldata weights) external;
+    function setDelegatedVoter(uint256 tokenId, address delegatedVoter) external;
 }
